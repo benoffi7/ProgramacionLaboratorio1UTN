8ae69dd41ee67aabbf0b982d0f89b06632d22fe3
TechMDQ <info@techmdq.com.ar>
2020-04-10 22:34:53 -0300

    Funciones - Ejemplos

---
+++ Comisión 9 - Docente Sergio Garguir/08 - Funciones - Ejemplos y Resoluciones/funciones1/main.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "pila.h"
+
+/// Prototipado de las funciones
+void miPrimerFuncion();
+int suma(int a, int b);
+int resta(int a, int b);
+float division(int a, int b);
+int multiplicacion(int a, int b);
+void intercambio(int a, int b);
+void intercambioPuntero(int *a, int *b);
+void cargarPila(Pila *p);
+void pasaPila(Pila *origen, Pila *destino);
+void copiaPila(Pila origen, Pila *destino);
+
+const int ALGO = 21;
+
+
+int main()
+{
+    Pila dada, pilita;
+    inicpila(&dada);
+    inicpila(&pilita);
+
+    cargarPila(&dada);
+    printf("\nPila dada");
+    mostrar(&dada);
+
+    pasaPila(&dada, &pilita);
+    printf("\nPila dada");
+    mostrar(&dada);
+    printf("\nPila pilita");
+    mostrar(&pilita);
+
+    copiaPila(pilita, &dada);
+    printf("\nPila dada");
+    mostrar(&dada);
+    printf("\nPila pilita");
+    mostrar(&pilita);
+
+    printf("Hello world!\n");
+    miPrimerFuncion();
+
+    int n1, n2, resultado;
+
+    printf("\n Ingrese un nro: ");
+    scanf("%d",&n1);
+    printf("\n Ingrese un nro: ");
+    scanf("%d",&n2);
+
+    resultado = suma(n1, n2); /// pasaje de parametros por copia
+
+    printf("\n\n El resultado de %d + %d es %d",n1, n2, resultado);
+
+
+    intercambio(n1, n2);
+    printf("\n Mostrando variables intercambiadas n1 %d y n2 %d", n1, n2);
+
+    intercambioPuntero(&n1, &n2);
+
+    printf("\n Mostrando variables intercambiadas n1 %d y n2 %d", n1, n2);
+
+    return 0;
+}
+
+
+/// Codigo de las funciones
+void miPrimerFuncion(){
+	printf("\n Esta es mi primer funcion");
+	printf("\n _______________________________________________");
+}
+
+int suma(int a, int b){
+    int total;
+
+    total = a + b;
+
+    return total;
+}
+
+void intercambio(int a, int b){
+    int tmp;
+    printf("\n Mostrando variables antes del intercambio a %d y b %d", a, b);
+    tmp = a;
+    a = b;
+    b = tmp;
+    printf("\n Mostrando variables intercambiadas a %d y b %d", a, b);
+}
+
+void intercambioPuntero(int *a, int *b){
+    int tmp;
+    printf("\n Mostrando variables antes del intercambio a %d y b %d", *a, *b);
+    tmp = *a; /// direccio de memoria de n1
+    *a = *b;
+    *b = tmp;
+    printf("\n Mostrando variables intercambiadas a %d y b %d", *a, *b);
+}
+
+void cargarPila(Pila *p){
+    char opcion;
+    int temperatura;
+
+	do{
+		printf("\n Ingrese temperatura: ");
+		scanf("%d", &temperatura);
+		apilar(p, temperatura);
+
+		printf("\n\n\t ESC para salir....");
+		opcion=getch();
+		system("cls");
+	}while(opcion!=27);
+}
+
+void pasaPila(Pila *origen, Pila *destino){
+    while(!pilavacia(origen)){
+        apilar(destino, desapilar(origen));
+    }
+}
+
+void copiaPila(Pila origen, Pila *destino){
+    while(!pilavacia(&origen)){
+        apilar(destino, desapilar(&origen));
+    }
+}
