11c3e75c707b51f78d489b0e7e52c0f14ba97d69
Gonzalo Benoffi <benoffi7@users.noreply.github.com>
2021-03-11 18:29:50 -0300

    2021

--- Comisión 2 - Docente Sergio Garguir/09 - Arreglos/pilasArreglos/miLib.h
+++
@@ -1,8 +0,0 @@
-#ifndef MILIB_H_INCLUDED
-#define MILIB_H_INCLUDED
-
-int cargaArregloInt(int a[], int v, int dim);
-void muestraArregloInt(int a[], int v);
-int cargaArregloOrdenadoInt(int a[], int v, int dim);
-
-#endif // MILIB_H_INCLUDED
