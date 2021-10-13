42d7267db8d446578009158fa0289df74cc23496
Sergio Garguir <sgarguir@mobydigital.com>
2021-10-13 19:03:56 -0300

    Com 9

---
+++ Comisión 9 - Docente Sergio Garguir/2021 - Programacion 2/2021-08-11 - Clase 02  - Practica Estructuras/clase02/celda.h
@@ -0,0 +1,12 @@
+#ifndef CELDA_H_INCLUDED
+#define CELDA_H_INCLUDED
+
+#include "listaEmpleados.h"
+
+typedef struct{
+    char cargo[20];
+    nodo* listaEmpleados;
+}stCelda;
+
+
+#endif // CELDA_H_INCLUDED
