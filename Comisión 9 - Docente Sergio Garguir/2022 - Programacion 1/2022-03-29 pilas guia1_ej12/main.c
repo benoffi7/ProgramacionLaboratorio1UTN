17107db1ba8b5e5856c1d1758a183fcd2a0b313b
Sergio Garguir <sergiogarguir@gmail.com>
2022-03-29 21:58:43 -0300

    2022-03-29

--- Comisión 9 - Docente Sergio Garguir/2022 - Programacion 1/2022-03-29 pilas guia1_ej12/main.c
+++ Comisión 9 - Docente Sergio Garguir/2022 - Programacion 1/2022-03-29 pilas guia1_ej12/main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pila.h"
+
 
 int main()
 {
-    printf("Hello world!\n");
+    Pila dada;
+    Pila modelo;
+    Pila aux;
+    Pila auxModelo;
+    Pila basura;
+
+    inicpila(&dada);
+    inicpila(&modelo);
+    inicpila(&aux);
+    inicpila(&auxModelo);
+    inicpila(&basura);
+
+    for(int i=0;i<10;i++){
+        apilar(&dada, rand()%10);
+    }
+    for(int i=0;i<15;i++){
+        apilar(&modelo, rand()%10);
+    }
+
+    printf("<<<<<<<<<<<<<<< pila dada >>>>>>>>>>>>>>>>>>>");
+    mostrar(&dada);
+
+    printf("<<<<<<<<<<<<<<< pila modelo >>>>>>>>>>>>>>>>>>>");
+    mostrar(&modelo);
+
+    while(!pilavacia(&modelo)){
+        while(!pilavacia(&dada)){
+            if(tope(&modelo)==tope(&dada)){
+                apilar(&basura, desapilar(&dada));
+            }else{
+                apilar(&aux, desapilar(&dada));
+            }
+        }
+
+        while(!pilavacia(&aux)){
+            apilar(&dada, desapilar(&aux));
+        }
+        apilar(&auxModelo, desapilar(&modelo));
+    }
+
+    while(!pilavacia(&auxModelo)){
+        apilar(&modelo, desapilar(&auxModelo));
+    }
+
+    printf("<<<<<<<<<<<<<<< pila dada >>>>>>>>>>>>>>>>>>>");
+    mostrar(&dada);
+
+    printf("<<<<<<<<<<<<<<< pila modelo >>>>>>>>>>>>>>>>>>>");
+    mostrar(&modelo);
+
     return 0;
 }
